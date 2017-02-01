fb094d3246cf5c6723ebb3c6ad76c9539bd3bbe8
ydy1128 <ydy1128@gmail.com>
2017-02-01 00:38:07 -0600

    local mongodb setup

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/frameitdb');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -27,6 +29,12 @@ pyshell.on('message', function (message) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
