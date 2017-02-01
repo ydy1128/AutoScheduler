b198ed6b9ea41235637de75f47ff9991df5235c9
ydy1128 <ydy1128@gmail.com>
2017-02-01 00:41:40 -0600

    route to db

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+    // var collection = db.get('videos');
+    // collection.find({}, function(err, videos){
+    //     if (err) throw err;
+    //   	res.json(videos);
+    // });
 });
 
 module.exports = router;
