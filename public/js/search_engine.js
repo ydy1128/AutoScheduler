2d9c34941a0f4b0a88aba3b9a540b8d237607447
ydy1128 <ydy1128@gmail.com>
2017-02-05 18:07:03 -0600

    search engine design

--- public/js/search_engine.js
+++ public/js/search_engine.js
@@ -1,4 +1,7 @@
 var search_engine = angular.module('searchEngine', []);
 search_engine.controller('searchEngineCtrl', function($scope){
 	// $scope.works = 'works'
-})
+
+});
+
+$(".search-section select").select2();
