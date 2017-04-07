def2dbac77e3402d6f9cd40a1ba320f3df0933f4
ydy1128 <ydy1128@gmail.com>
2017-04-06 21:44:41 -0500

    config update

--- app.js
+++ app.js
@@ -17,8 +17,12 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
+
 // DB------------------------------------------------------
-mongoose.connect("mongodb://heroku_d4bw5531:frameitDev78@ds117899.mlab.com:17899/heroku_d4bw5531");
+var uristring = process.env.MONGOLAB_URI ||
+			    process.env.MONGOHQ_URL ||
+			    "mongodb://localhost/frameitdb";
+mongoose.connect(uristring);
 var db = mongoose.connection;
 db.once('open', function(){
 	console.log('DB Connected');
